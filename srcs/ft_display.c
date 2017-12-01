7d8e547b042197355371a5e62ec0bf0b49541345
Andrew HUANG <anhuang@e3r7p3.42.fr>
2017-12-01 16:12:06 +0100

    Maj ft_display.c

--- srcs/ft_display.c
+++ srcs/ft_display.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 12:22:46 by anhuang           #+#    #+#             */
-/*   Updated: 2017/11/28 17:23:25 by anhuang          ###   ########.fr       */
+/*   Updated: 2017/12/01 16:09:06 by anhuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,124 +25,66 @@ void			ft_putstr(char const *s)
 	}
 }
 
-char			*ft_display_t(char *out, int x, int i, t_t *input)
+static char		*ft_display_t(char *out, int x, unsigned char size, t_t *input)
 {
-	int		bit;
-	int		dif;
+	int				bit;
+	int				dif;
+	unsigned char	i;
+	int				man;
+	int				a;
 
+	a = 0;
+	man = 0;
+	i = 0;
 	dif = 0;
 	bit = 0;
 	while (bit != 4)
 	{
-		if (i % 4 == 0 && i != 0)
-			dif += x - 4;
-		if (ft_state_bit(input->map, i) == 1)
+		if (out[i] == '\n')
 		{
-			out[i + input->x + input->y * x + dif] = input->letter;
+			a += 16 - size;
+			dif++;
+			i++;
+		}
+		if (dif == 4)
+		{
+			man++;
+			a = 0;
+			dif = 0;
+		}
+		if (ft_bitat(input->map[man], a) == 1)
+		{
+			out[i] = input->letter;
 			bit++;
 		}
-		if (out[i] == '\n')
-			i++;
+		a++;
 		i++;
 	}
 	return (out);
 }
 
-char			*ft_display(t_t *input)
+char			*ft_display(t_t *input, unsigned char size)
 {
 	char	*out;
 	int		i;
 	int		x;
 
 	i = 0;
-	if (!(out = (char*)malloc((input->x * input->x + input->x))))
+	if (!(out = (char*)malloc((size * size + size))))
 		return (NULL);
-	ft_memset(out, '.', input->x * input->x + input->x);
-	while (i != (input->x * input->x + input->x))
+	ft_memset(out, '.', size * size + size);
+	while (i != (size * size + size))
 	{
-		if ((i + 1) % (input->x + 1) == 0 && i != 0)
+		if ((i + 1) % (size + 1) == 0 && i != 0)
 			out[i] = '\n';
 		i++;
 	}
 	out[i] = '\0';
-	x = input->x + 1;
-	while (input->letter != 'A')
+	while (input->letter != 0)
 	{
-		input--;
-		ft_display_t(out, x, 0, input);
+		ft_display_t(out, 0, size, input);
+		input++;
 	}
 	ft_putstr(out);
 	return (out);
-}
-
-int main ()
-{
-	t_t toto[9];
-
-	toto[0].map = 99;
-	toto[0].x = 0;
-	toto[0].y = 0;
-	toto[0].dx = 3;
-	toto[0].dy = 2;
-	toto[0].letter = 'A';
-
-	toto[1].map = 99;
-	toto[1].x = 2;
-	toto[1].y = 0;
-	toto[1].dx = 3;
-	toto[1].dy = 2;
-	toto[1].letter = 'B';
-
-	toto[2].map = 15;
-	toto[2].x = 0;
-	toto[2].y = 2;
-	toto[2].dx = 4;
-	toto[2].dy = 1;
-	toto[2].letter = 'C';
-
-	toto[3].map = 547;
-	toto[3].x = 4;
-	toto[3].y = 0;
-	toto[3].dx = 2;
-	toto[3].dy = 3;
-	toto[3].letter = 'D';
-
-	toto[4].map = 113;
-	toto[4].x = 0;
-	toto[4].y = 4;
-	toto[4].dx = 3;
-	toto[4].dy = 2;
-	toto[4].letter = 'E';
-
-	toto[5].map = 51;
-	toto[5].x = 1;
-	toto[5].y = 3;
-	toto[5].dx = 2;
-	toto[5].dy = 2;
-	toto[5].letter = 'F';
-
-	toto[6].map = 4369;
-	toto[6].x = 4;
-	toto[6].y = 2;
-	toto[6].dx = 1;
-	toto[6].dy = 4;
-	toto[6].letter = 'G';
-
-	toto[7].map = 0;
-	toto[7].x = 6;
-	toto[7].y = 0;
-	toto[7].dx = 0;
-	toto[7].dy = 0;
-	toto[7].letter = 0;
-
-	toto[8].map = 0;
-	toto[8].x = 6;
-	toto[8].y = 0;
-	toto[8].dx = 0;
-	toto[8].dy = 0;
-	toto[8].letter = 0;
-
-
-	ft_display(toto + 7);
-	return(0);
 }
